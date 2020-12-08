8f18810bd09567d328b2bb33c6144125edc55aa3
Scott Young <scott@sjy.id.au>
2020-12-08 22:45:12 +1100

    day 2 (updated)

--- day2.js
+++ day2.js
@@ -1,3 +1,5 @@
-input.map(line => line.split(/:? /)).filter(([range, char, pass]) => (([min, max, count]) => count >= min && count <= max)(range.split('-').concat([Array.from(pass).filter(c => c == char).length]))).length
+// part 1
+document.body.innerText.trim().split('\n').map(line => line.split(/:? /)).filter(([range, char, pass]) => (([min, max, count]) => count >= min && count <= max)(range.split('-').concat([Array.from(pass).filter(c => c == char).length]))).length
 
-input.map(line => line.split(/:? /)).filter(([index, char, pass]) => (([a, b]) => a ^ b)(index.split('-').map(i => pass[i-1] == char ? 1 : 0))).length
+// part 2
+document.body.innerText.trim().split('\n').map(line => line.split(/:? /)).filter(([index, char, pass]) => (([a, b]) => a ^ b)(index.split('-').map(i => pass[i-1] == char ? 1 : 0))).length
