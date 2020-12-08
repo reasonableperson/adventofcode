4e8591d21fa883f0f490f4a8ea8009565f04b716
Scott Young <scott@sjy.id.au>
2020-12-08 22:48:19 +1100

    day 3 (updated)

--- day3.js
+++ day3.js
@@ -1,10 +1,5 @@
 // part 1
-input.filter((row, i) => row[3*i % row.length] == '#').length
+document.body.innerText.trim().split('\n').filter((row, i) => row[3*i % row.length] == '#').length
 
 // part 2
-[[1,1], [3,1], [5,1], [7,1], [1,2]]
-  .map(([r, d]) =>
-    input.filter((row, i) =>
-      i*d < input.length && input[i*d][i*r % row.length] == '#')
-    .length)
-  .reduce((a, b) => a * b)
+(input => [[1,1], [3,1], [5,1], [7,1], [1,2]].map(([r, d]) => input.filter((row, i) => i*d < input.length && input[i*d][i*r % row.length] == '#').length).reduce((a, b) => a * b))(document.body.innerText.trim().split('\n'))
