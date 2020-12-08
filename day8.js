0e45688c054baf2a80a886de08fbe994e8a28f10
Scott Young <scott@sjy.id.au>
2020-12-08 23:28:18 +1100

    day 8 (updated)

--- day8.js
+++ day8.js
@@ -1,10 +1,10 @@
 // common
-exec = ([i,op,arg],acc) => ({jmp: (arg,i,acc) => [i+arg,acc], acc: (arg,i,acc) => [i+1,acc+arg], nop: (_,i,acc) => [i+1,acc]})[op](arg,i,acc)
+exec = ([i,op,arg],acc) => ({jmp: (i,arg,acc) => [i+arg,acc], acc: (i,arg,acc) => [i+1,acc+arg], nop: (i,_,acc) => [i+1,acc]})[op](i,arg,acc)
 read = (tape,until,then) => (rec = ([seen=new Set, i=0, acc=0]) => until(seen,i) ? then(i,acc) : rec([seen.add(i), ...exec(tape[i],acc)]))([])
 tape = document.body.innerText.trim().split('\n').map(s => s.split(' ')).map(([op,arg],i) => [i,op,parseInt(arg,10)])
 
 // part 1
-read(tape,(seen,i) => seen.has(i), (i,acc) => acc)
+read(tape, (seen,i) => seen.has(i), (i,acc) => acc)
 
 // part 2
 tape.filter(([i,op,arg]) => /p/.test(op))
