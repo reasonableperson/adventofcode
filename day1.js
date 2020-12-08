06878d93f6bc2de3159ced8d048a2633f6100070
Scott Young <scott@sjy.id.au>
2020-12-08 22:41:51 +1100

    day 1 (updated)

--- day1.js
+++ day1.js
@@ -1,1 +1,5 @@
-input.map(x => [x, input.map(y => 2020 - x - y).filter(z => input.includes(z))]).filter(x => x[1].length).reduce((p, x) => p * x[0], 1)
+// part 1
+(xs => (x => 2020*x-x*x)(xs.map(x => 2020-x).find(x => xs.includes(x))))(document.body.innerText.trim().split('\n').map(n => parseInt(n,10)))
+
+// part 2
+(xs => xs.map(x => [x, xs.map(y => 2020 - x - y).filter(z => xs.includes(z))]).filter(x => x[1].length).reduce((p,x) => p*x[0],1))(document.body.innerText.trim().split('\n').map(n => parseInt(n,10)))
