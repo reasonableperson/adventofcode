bb8596366b972447d4855078112de3b1e4026598
Scott Young <scott@sjy.id.au>
2021-12-06 09:33:21 +1100

    day 4

---
+++ 2021/day4.js
@@ -0,0 +1,34 @@
+input = document.body.innerText.trim().split('\n')
+
+draws = input[0].split(',')
+
+boards = [...Array((input.length - 1) / 6).keys()]
+  .map(i => input.slice(2 + 6*i, 7 + 6*i))
+  .map(b => b.map(l => l.split(/ +/).filter(x => x).map(Number)))
+
+transpose = board => board.map((l,i) => board.map(l => l[i]))
+
+win = board =>
+  board.concat(transpose(board)).some(l => l.every(i => i[0] == '*'))
+
+score = (board, n) => board
+  .flat().reduce((a,b) => typeof b == "string" ? a : a + b, 0) * n
+
+play = (boards, n) =>
+  boards.map(b => b.map(l => l.map(i => i == n ? '*' + i : i)))
+
+solve = (boards, endgame) => {
+  winners = []
+  for (i = 0; i < draws.length; i++) {
+    boards = play(boards, draws[i])
+	if (boards.find(win)) winners = winners.concat(boards.filter(win))
+	boards = boards.filter(b => !win(b))
+	console.log(draws[i], boards.length, winners.length)
+	if (winners.length == endgame)
+	  return score(winners.pop(), draws[i])
+  }
+}
+
+part1 = solve(boards, 1) // 51034
+
+part2 = solve(boards, boards.length) // 5434
