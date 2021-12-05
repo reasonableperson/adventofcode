144fa6c4338d7ba31eea5cc0f294f0f50a006813
Scott Young <scott@sjy.id.au>
2021-12-06 10:24:29 +1100

    revise day 5

--- 2021/day5.js
+++ 2021/day5.js
@@ -1,23 +1,20 @@
 input = document.body.innerText.trim().split('\n')
   .map(pair => pair.split(/,| -> /).map(Number))
 
-log = (x, y) => {
-  point = x + "," + y
-  if (point in points) points[point] += 1
-  else points[point] = 1
-}
+update = (map, [x, y]) => map.set(x+","+y, map.get(x+","+y) + 1 || 1)
 
-traverse = ([x1, y1, x2, y2]) => {
-  var [dx, dy] = [Math.sign(x2 - x1), Math.sign(y2 - y1)]
-  for (i = 0; i <= Math.max(Math.abs(x2-x1), Math.abs(y2-y1)); i++)
-    log(x1 + i*dx, y1 + i*dy)
+traverse = (map, [x1, y1, x2, y2]) => {
+  [dx, dy] = [Math.sign(x2 - x1), Math.sign(y2 - y1)]
+  n = Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1)) + 1
+  return [...Array(n).keys()]
+    .map(i => [x1 + i*dx, y1 + i*dy])
+    .reduce(update, map)
 }
 
 straight = ([x1, y1, x2, y2]) => x1 == x2 || y1 == y2
-points = {}
-input.filter(straight).forEach(traverse)
-part1 = Object.values(points).filter(x => x > 1).length // 5698
 
-points = {}
-input.forEach(traverse)
-part2 = Object.values(points).filter(x => x > 1).length // 15463
+part1 = Array.from(input.filter(straight).reduce(traverse, new Map).values())
+  .filter(x => x > 1).length // 5698
+
+part2 = Array.from(input.reduce(traverse, new Map).values())
+  .filter(x => x > 1).length // 15463
