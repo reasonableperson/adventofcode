8cc2e32342aea98d05f60b21c2063f17ec976c25
Scott Young <scott@sjy.id.au>
2021-12-09 19:15:33 +1100

    revise day 9

--- 2021/day9.js
+++ 2021/day9.js
@@ -1,34 +1,23 @@
 input = document.body.innerText.trim().split('\n').map(line => [...line].map(Number))
 
-// common
+neighbours = ([i,j], a) => [[i-1,j], [i+1,j], [i,j-1], [i,j+1]]
+  .filter(([i,j]) => a[i]?.[j] != undefined)
 
-low = (i, j, a) => [a[i-1]?.[j], a[i+1]?.[j], a[i][j-1], a[i][j+1]]
-  .every(adjacent => adjacent == undefined || a[i][j] < adjacent)
+low = (i, j, a) => neighbours([i,j], a).every(([i_,j_]) => a[i_][j_] > a[i][j])
 
-// part 1
+basins = input.map((r,i,a) => r.map((x,j) => low(i,j,a) ? [i,j] : null)).flat().filter(x=>x)
 
-part1 = input.map((r,i,a) => r.filter((x,j) => low(i,j,a)))
-  .flat().map(x => x+1).reduce((a,b) => a+b) // 478
+part1 = basins.reduce((a, [i,j]) => a + input[i][j] + 1, 0) // 478
 
-// part 2
-
-neighbours = ([i,j], a) => [[i-1,j,a[i][j]], [i+1,j,a[i][j]], [i,j-1,a[i][j]], [i,j+1,a[i][j]]]
-  .filter(([i,j,x]) => x < a[i]?.[j] && a[i]?.[j] < 9)
-  .map(([i,j]) => [i,j,a[i][j]])
-
-lows = input.map((r,i,a) => r.map((x,j) => low(i,j,a) ? [i,j] : null))
-  .flat().filter(x=>x)
-  
-explore = ([i, j], a, queue = [], map = [...Array(input.length)].map(_ => Array(input.length))) => {
-  map[i][j] = true
-  queue = queue.concat(neighbours([i,j],a))
+explore = ([i, j], a, queue = [], seen = (n => [...Array(n)].map(_ => Array(n)))(input.length)) => {
+  seen[i][j] = true
+  queue = queue.concat(neighbours([i,j],a).filter(([i_,j_]) => a[i][j] < a[i_][j_] && a[i_][j_] < 9 ))
   while (queue.length > 0) {
     [i, j] = queue.pop()
-	if (!map[i]?.[j])
-	  map = explore([i,j], a, queue, map)
+    if (!seen[i]?.[j]) seen = explore([i,j], a, queue, seen)
   }
-  return map
+  return seen
 }
 
-lows.map(l => explore(l,input).flat().filter(x=>x).length)
+part2 = basins.map(l => explore(l,input).flat().filter(x=>x).length)
   .sort((a,b)=>a<b).slice(0,3).reduce((a,b)=>a*b) // 1327014
