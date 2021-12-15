796f2bb9bc0cc6514e214cac1fb35cef3ff968b5
Scott Young <scott@sjy.id.au>
2021-12-16 10:07:56 +1100

    day 15 part 2

--- 2021/day15.js
+++ 2021/day15.js
@@ -1,45 +1,52 @@
-// var input = `1163751742
-// 1381373672
-// 2136511328
-// 3694931569
-// 7463417111
-// 1319128137
-// 1359912421
-// 3125421639
-// 1293138521
-// 2311944581`.split('\n').map(l => l.split('').map(Number))
-
-var input = document.body.innerText.trim().split('\n').map(l => l.split('').map(Number))
-
-var coords = [...Array(input.length)].map((_,i,a) => a.map((_,j,a) => [i,j])).flat()
-
-var neighbours = ([i,j]) => [[i,j-1],[i+1,j],[i,j+1],[i-1,j]].filter(ij => ij.every(i => i>=0 && i<input.length)).map(([i,j]) => i+','+j)
+var input = (await Deno.readTextFile('input')).trim().split('\n').map(l => l.split('').map(Number))
 
 var solve = input => {
-  var i = 0
+  var steps = 0
   var current = '0,0'
   var destination = (l => (l-1)+','+(l-1))(input.length)
+  var coords = [...Array(input.length)].map((_,i,a) => a.map((_,j,a) => [i,j])).flat()
   var distances = new Map(coords.map(([i,j]) => [i+','+j, i==0 && j==0 ? 0 : Infinity]))
-  var unvisited = new Set(coords.map(([i,j]) => i+','+j))
-  unvisited.delete('0,0')
+  var neighbours = ([i,j]) => [[i,j-1],[i+1,j],[i,j+1],[i-1,j]].filter(ij => ij.every(i => i>=0 && i<input.length)).map(([i,j]) => i+','+j)
+  var unvisited = ['0,0']
+  var visited = new Set
 
-  while (unvisited.has(destination) && i<50000) {
-    i++
-    //console.log(i, 'current', current, 'd', distances.get(current))
+  while ((current = unvisited.shift())) {
+    steps++
+	//console.log()
+    console.log(steps, 'visiting', current, distances.get(current))
     neighbours(current.split(',').map(Number)).forEach(ij => {
-	  if (!unvisited.has(ij)) return
+	  if (visited.has(ij)) {
+		  //console.log('  skip', ij)
+		  return
+	  }
       var [i,j] = ij.split(',').map(Number)
-	  //console.log('set', ij, distances.get(current), input[i][j])
-      distances.set(ij, Math.min(distances.get(ij), distances.get(current) + input[i][j]))
+	  var d = distances.get(current) + input[i][j]
+	  var old_pos = unvisited.findIndex(x => x == ij)
+      if (d < distances.get(ij)) {
+		//console.log('  set', ij, d)
+		distances.set(ij, d)
+		var new_pos = unvisited.findIndex(x => distances.get(x) > d)
+		if (new_pos != -1) unvisited.splice(new_pos, 0, ij)
+		else unvisited.push(ij)
+	  } else if (old_pos == -1) {
+		var new_pos = unvisited.findIndex(x => distances.get(x) > d)
+		if (new_pos != -1) unvisited.splice(new_pos, 0, ij)
+	  }
     })
-    unvisited.delete(current)
-    //console.log(i, 'unvisited', unvisited)
+	visited.add(current)
+	//console.log('visited', visited)
+    //console.log('unvisited', unvisited.map(ij => [ij, distances.get(ij)]))
     //console.log('distances', distances)
-    current = [...unvisited].reduce((prev,next) => distances.get(next) < distances.get(prev) ? next : prev, destination)
+	//console.log('candidates', [...unvisited].map(x => [x, distances.get(x)]))
   }
   //console.log('result', distances)
-  //console.log(i, [...Array(input.length)].map((_,i,a) => a.map((_,j,a) => distances.get(i+','+j)).join(' ')).join('\n'))
-  console.log(i, distances.get(destination))
+  // console.log([...Array(input.length)].map((_,i,a) => a.map((_,j,a) => distances.get(i+','+j)).join(' ')).join('\n'))
+  //console.log(distances)
+  console.log(steps, distances.get(destination))
 }
 
-solve(input)
+// solve(input) // 373
+
+var input2 = [...Array(5)].map((_,i) => input.map(l => [...Array(5)].map((_,j) => l.map(n => (n+i+j)%9 == 0 ? 9 : (n+i+j)%9 )).flat())).flat()
+
+solve(input2) // 4956 is too high
