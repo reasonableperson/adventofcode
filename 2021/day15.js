273994b5b90120eb1f1d7638466ef4ed22702705
Scott Young <scott@sjy.id.au>
2021-12-16 20:25:38 +1100

    revise day 15

--- 2021/day15.js
+++ 2021/day15.js
@@ -12,18 +12,13 @@ var solve = input => {
 
   while ((current = unvisited.shift())) {
     steps++
-	//console.log()
-    console.log(steps, 'visiting', current, distances.get(current))
+    console.log(steps, 'visiting', current)
     neighbours(current.split(',').map(Number)).forEach(ij => {
-	  if (visited.has(ij)) {
-		  //console.log('  skip', ij)
-		  return
-	  }
+	  if (visited.has(ij)) return
       var [i,j] = ij.split(',').map(Number)
 	  var d = distances.get(current) + input[i][j]
 	  var old_pos = unvisited.findIndex(x => x == ij)
       if (d < distances.get(ij)) {
-		//console.log('  set', ij, d)
 		distances.set(ij, d)
 		var new_pos = unvisited.findIndex(x => distances.get(x) > d)
 		if (new_pos != -1) unvisited.splice(new_pos, 0, ij)
@@ -34,19 +29,10 @@ var solve = input => {
 	  }
     })
 	visited.add(current)
-	//console.log('visited', visited)
-    //console.log('unvisited', unvisited.map(ij => [ij, distances.get(ij)]))
-    //console.log('distances', distances)
-	//console.log('candidates', [...unvisited].map(x => [x, distances.get(x)]))
   }
-  //console.log('result', distances)
-  // console.log([...Array(input.length)].map((_,i,a) => a.map((_,j,a) => distances.get(i+','+j)).join(' ')).join('\n'))
-  //console.log(distances)
-  console.log(steps, distances.get(destination))
+  return distances.get(destination)
 }
 
-// solve(input) // 373
+console.log('part1', solve(input)) // 373
 
-var input2 = [...Array(5)].map((_,i) => input.map(l => [...Array(5)].map((_,j) => l.map(n => (n+i+j)%9 == 0 ? 9 : (n+i+j)%9 )).flat())).flat()
-
-solve(input2) // 4956 is too high
+console.log('part2', solve([...Array(5)].map((_,i) => input.map(l => [...Array(5)].map((_,j) => l.map(n => (n+i+j)%9 == 0 ? 9 : (n+i+j)%9 )).flat())).flat())) // 2868
