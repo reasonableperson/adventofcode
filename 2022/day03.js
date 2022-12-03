c3f5da6d8237d28fb2690e27005edf71ba2ea233
Scott Young <s@sjy.au>
2022-12-03 14:52:17 +0800

    revise day 3

--- 2022/day03.js
+++ 2022/day03.js
@@ -2,9 +2,10 @@ input = document.body.innerText.split('\n').filter(x=>x)
 
 priority = chr => chr.charCodeAt() - (chr > "Z" ? 96 : 38)
 
-common = strs => strs.reduce((arr,str) => arr.filter(chr => str.includes(chr)), [...strs[0]])[0] 
+intersect = strs => strs.reduce((arr, str) => arr.filter(chr => str.includes(chr)), [...strs[0]])[0]
 
-part1 = input.reduce((sum, l) => sum + priority(common([l.slice(0, l.length/2), l.slice(l.length/2)])), 0) // 7742
+chunks = (arr, n) => Array.from({length: arr.length/n}, (_, i) => arr.slice(n*i, n*i+n))
 
-part2 = Array.from({length: input.length/3}, (_, i) => input.slice(3*i, 3*i+3))
-  .reduce((a,b) => a + priority(common(b)), 0) // 2276
+part1 = input.reduce((sum, line) => sum + priority(intersect(chunks(line, line.length/2))), 0) // 7742
+
+part2 = chunks(input, 3).reduce((sum, group) => sum + priority(intersect(group)), 0) // 2276
