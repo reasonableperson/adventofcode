c1e5438c0c0148cba2f5d804321b9d9e6b685d7a
Scott Young <s@sjy.au>
2022-12-05 15:28:48 +0800

    day 5 rev 4

--- 2022/day05.js
+++ 2022/day05.js
@@ -3,15 +3,14 @@ input = document.body.innerText
 
 stacks = input[0].slice(0,-1).reduce((result, _, i, arr) => 
   result.map((s, j) => s += arr[arr.length-1-i].match(/[A-Z]|    /g)[j] || ''),
-  Array(9).fill("")).map(s => s.replaceAll(' ', ''))
+  Array(9).fill('')).map(s => s.replaceAll(' ', ''))
 
-steps = input[1].map(s => s.match(/[0-9]+/g).map(d => parseInt(d)))
+steps = input[1].map(s => s.match(/[0-9]+/g).map(d => parseInt(d)-1))
 
 solve = pt => (result, [n, from, to]) => {
-  var chunk = result[from-1].slice([result[from-1].length - n])
-  if (pt == 1) chunk = [...chunk].reverse().join('')
-  result[to-1] += chunk
-  result[from-1] = result[from-1].slice(0, result[from-1].length - n)
+  var chunk = result[from].slice(-1-n)
+  result[to] += pt == 1 ? [...chunk].reverse().join('') : chunk
+  result[from] = result[from].slice(0,-1-n)
   return result
 }
 
