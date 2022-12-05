738a8dcbb63697133bc480d6f50da69bd9936942
Scott Young <s@sjy.au>
2022-12-05 09:14:43 +0800

    revise day 2

--- 2022/day02.js
+++ 2022/day02.js
@@ -1,6 +1,6 @@
 input = document.body.innerText.split('\n').filter(x=>x)
   .map(line => [line[0].charCodeAt()-65, line[2].charCodeAt()-88])
 
-part1 = input.reduce((total, [them, us]) => (us-them == 0 ? 3 : us-them == 1 || us-them == -2 ? 6 : 0) + us + 1 + total, 0) // 10994
+part1 = input.reduce((total, [them, us]) => (us==them ? 3 : [1,-2].includes(us-them) ? 6 : 0) + us + 1 + total, 0) // 10994
 
-part2 = input.reduce((total, [them, outcome]) => [(them-1) < 0 ? 3 : them, them+4, them+1 < 3 ? them+8 : 7][outcome] + total, 0) // 12526
+part2 = input.reduce((total, [them, outcome]) => [them<1?3:them, them+4, them<2?them+8:7][outcome] + total, 0) // 12526
