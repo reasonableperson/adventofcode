a966cd3ebab99b1024d8e1a7131fce1f25acc906
Scott Young <s@sjy.au>
2022-12-09 01:14:30 +0800

    day 8 rev 1

--- 2022/day08.js
+++ 2022/day08.js
@@ -4,23 +4,16 @@ side = [...Array(input.length)].map((_,i) => i)
 
 coords = side.map(i => side.map(j => [i,j])).flat()
 
-part1 = coords.reduce((sum, [i,j]) => {
-  up = side.slice(0,i).every(i_ => input[i_][j] < input[i][j])
-  left = side.slice(0,j).every(j_ => input[i][j_] < input[i][j])
-  right = side.slice(j+1).every(j_ => input[i][j_] < input[i][j])
-  down = side.slice(i+1).every(i_ => input[i_][j] < input[i][j])
-  return [up, left, right, down].some(x => x) ? sum + 1 : sum
-}, 0) // 1801
+bearings = (i,j) => [
+  side.slice(0,i).reverse().map(i_ => input[i_][j]), // up
+  side.slice(0,j).reverse().map(j_ => input[i][j_]), // left
+  side.slice(i+1).map(i_ => input[i_][j]), // down
+  side.slice(j+1).map(j_ => input[i][j_])] // right
 
-until = (arr, func) => {
-  var i = arr.findIndex(func)
-  return i == -1 ? arr : arr.slice(0, Math.min(i + 1, arr.length-1))
-}
+part1 = coords.filter(([i,j]) => bearings(i,j).some(b => b.every(ij => ij < input[i][j]))).length // 1801
 
-part2 = Math.max(...coords.map(([i,j]) => {
-  up = until(side.slice(0,i).reverse(), i_ => input[i_][j] >= input[i][j]).map(i_ => input[i_][j])
-  left = until(side.slice(0,j).reverse(), j_ => input[i][j_] >= input[i][j]).map(j_ => input[i][j_])
-  right = until(side.slice(j+1), j_ => input[i][j_] >= input[i][j]).map(j_ => input[i][j_])
-  down = until(side.slice(i+1), i_ => input[i_][j] >= input[i][j]).map(i_ => input[i_][j])
-  return up.length * left.length * right.length * down.length
-})) // 209880
+until = (arr, func) => (i => i == -1 ? arr : arr.slice(0, Math.min(i + 1, arr.length)))(arr.findIndex(func))
+
+score = ([i,j]) => bearings(i,j).map(b => until(b, ij => ij >= input[i][j]).length).reduce((a,b) => a*b, 1)
+
+part2 = Math.max(...coords.map(score)) // 209880
