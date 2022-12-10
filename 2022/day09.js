e95f7372d3baf63b8949fd44349944d219b0e951
Scott Young <s@sjy.au>
2022-12-10 10:19:32 +0800

    day 9 rev 1

--- 2022/day09.js
+++ 2022/day09.js
@@ -13,38 +13,21 @@ moves = {
   RU: ({x,y}) => ({x: x+1, y: y+1}),
 }
 
-plot = rope => {
-  var size = 20
-  var arr = [...Array(size)].map((_,j) => [...Array(size)].map((_,i) => '.'))
-  rope.reverse().forEach(({x,y}, i_) => {
-    var i = rope.length - i_ - 1
-    arr[size - (y+5) - 1][(x+5)] = i == 0 ? 'H' : i
-  })
-  return arr.map(line => line.join('')).join('\n  ')
-}
-
 move = ({rope, visited}, [dir, n]) => {
-  //console.log(' ', plot(rope))
-  //console.log('  move', dir, n, rope.reverse())
   while (n > 0) {
-    //console.log('rope', rope.map(({x,y}) => `${x},${y}`))
     rope.forEach(({x,y}, i, a) => {
       if (i==0) a[0] = moves[dir]({x,y})
       else {
-        //console.log('  rope', a.map(({x,y}) => `${x},${y}`))
         var [dx, dy] = [a[i-1].x - x, a[i-1].y - y]
         tailDir = ''
         if (dx > 1 || dx > 0 && manhattan(dx, dy) > 2) tailDir += 'R'
         else if (dx < -1 || dx < 0 && manhattan(dx, dy) > 2) tailDir += 'L'
         if (dy > 1 || dy > 0 && manhattan(dx, dy) > 2) tailDir += 'U'
         else if (dy < -1 || dy < 0 && manhattan(dx, dy) > 2) tailDir += 'D'
-        //console.log('  dx', dx, 'dy', dy, '  tailDir', tailDir)
         a[i] = tailDir ? moves[tailDir]({x,y}) : {x,y}
       }
     })
-    //console.log('  dx', dx, 'dy', dy, 'tailDir', tailDir)
     visited.add(rope.slice(-1).map(({x,y}) => `${x},${y}`)[0])
-    //console.log('  visited', visited)
     n--
   }
   return {rope, visited}
